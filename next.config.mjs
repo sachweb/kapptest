e3d3c6a1e8581bfc28c09f8fc6bdfe8301842d5f
sachweb <ssmewada1423@gmail.com>
2024-08-26 11:50:21 +0530

    s

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,9 @@
 const nextConfig = {
     output: 'export',
     trailingSlash: true, // Optional: Ensures all URLs have a trailing slash (e.g., /about/ instead of /about)
-  basePath: '',
+    basePath: '/https://github.com/sachweb/kapptest', // e.g. '/kavach-app'
+    assetPrefix: '/https://github.com/sachweb/kapptest/',
+  
     images: {
         unoptimized: true,
  }
